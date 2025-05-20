c412de656aa57dd01106d6576fd4c4dc5b0f3e76
Chris Nielubowicz <nielubowicz@gmail.com>
2025-05-20 10:53:27 -0400

    Add correct long-press handling

--- metrognome/Views/BPMView.swift
+++ metrognome/Views/BPMView.swift
@@ -3,45 +3,84 @@ import SwiftUI
 struct BPMView: View {
     @Binding var bpm: Int
     @State private var timer: Timer?
+    @GestureState private var minusState = false
+    @GestureState private var plusState = false
     
     var body: some View {
         HStack {
-            Button {
-                bpm = bpm - 1
-            } label: {
+            Button {}
+            label: {
                 Label("", systemImage: "minus.circle")
                     .frame(maxWidth: .infinity, maxHeight: .infinity)
             }
             .contentShape(Rectangle())
             .padding(.horizontal)
+            .highPriorityGesture(
+                TapGesture().onEnded({
+                    bpm = bpm - 1
+                })
+            )
+            .simultaneousGesture(
+                LongPressGesture(minimumDuration: .infinity, maximumDistance: .infinity)
+                    .updating($minusState, body: { current, gesture, _ in
+                        gesture = current
+                    })
+                    .onChanged({ pressed in
+                        timer = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { _ in
+                            checkMinusState()
+                        }
+                    })
+                    .onEnded({ pressed in
+                        stopChangingTempo()
+                    })
+            )
             
             Spacer(minLength: 4)
             Text(bpm.formatted())
             Spacer(minLength: 4)
             
-            Button {
-                bpm = bpm + 1
-            } label: {
+            Button {} label: {
                 Label("", systemImage: "plus.circle")
                     .frame(maxWidth: .infinity, maxHeight: .infinity)
             }
             .frame(maxWidth: .infinity, maxHeight: .infinity)
             .contentShape(Rectangle())
             .padding(.horizontal)
+            .highPriorityGesture(
+                TapGesture().onEnded({
+                    bpm = bpm + 1
+                })
+            )
+            .simultaneousGesture(
+                LongPressGesture(minimumDuration: .infinity, maximumDistance: .infinity)
+                    .updating($plusState, body: { current, gesture, _ in
+                        gesture = current
+                    })
+                    .onChanged({ pressed in
+                        timer = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { _ in
+                            checkPlusState()
+                        }
+                    })
+                    .onEnded({ pressed in
+                        stopChangingTempo()
+                    })
+            )
         }
     }
     
-    private func startIncreasingTempo() {
-        stopChangingTempo()
-        timer = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { _ in
-            bpm += 1
-        }
-    }
-    
-    private func startDecreasingTempo() {
-        stopChangingTempo()
-        timer = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { _ in
+    private func checkMinusState() {
+        if minusState {
             bpm -= 1
+        } else {
+            stopChangingTempo()
+        }
+    }
+    
+    private func checkPlusState() {
+        if plusState {
+            bpm += 1
+        } else {
+            stopChangingTempo()
         }
     }
     
